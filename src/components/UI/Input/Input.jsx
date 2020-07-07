a3b5e3d3664534a0a17a0e92003dc5d3626b9fc8
Sami H <samih90@gmail.com>
2020-07-07 13:58:36 +0300

    continued to work on forms

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React from 'react';
 
-import classes from './Input.module.css'
+import classes from './Input.module.css';
 
 const input = (props) => {
+  let inputElement = null;
 
-  
+  switch (props.elementType) {
+    case 'input':
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          value={props.value}
+          {...props.elementConfig}
+          onChange={props.handleChange}
+        />
+      );
+      break;
+    case 'select':
+      inputElement = (
+        <select
+          className={classes.InputElement}
+          value={props.value}
+          onChange={props.handleChange}
+        >
+          {props.elementConfig.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          value={props.value}
+          {...props.elementConfig}
+          onChange={props.handleChange}
+        />
+      );
+      break;
+  }
   return (
-  <div className={classes.Input}>
-    <label className={classes.Label}>
-      {props.label}
-    </label>
-    <input className={classes.InputElement} {...props} />
-  </div>
-)}
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
 
 export default input;
--- src/containers/Checkout/ContactInfo/ContactInfo.jsx
+++ src/containers/Checkout/ContactInfo/ContactInfo.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import Button from '../../../components/UI/Button/Button';
@@ -6,19 +6,39 @@ import Input from '../../../components/UI/Input/Input';
 import classes from './ContactInfo.module.css';
 import myAxios from '../../../axios-orders';
 
+const formHelper = (elType, elConfType, elConfHolder) => {
+  return {
+    elementType: elType,
+    elementConfig: {
+      type: elConfType,
+      placeholder: elConfHolder,
+    },
+    value: '',
+  };
+};
+
 class ContactInfo extends Component {
   state = {
     loading: false,
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
-    }
-  }
+    orderForm: {
+      name: formHelper('input', 'text', 'your name'),
+      email: formHelper('input', 'email', 'your email'),
+      street: formHelper('input', 'text', 'street'),
+      postalCode: formHelper('input', 'text', 'postal code'),
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'home delivery', displayValue: 'home delivery' },
+            { value: 'pick up', displayValue: 'pick up' },
+          ],
+        },
+      },
+    },
+  };
 
   orderHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const order = {
       ingredients: this.props.ingredients,
@@ -39,26 +59,54 @@ class ContactInfo extends Component {
       .post('/orders.json', order)
       .then((resp) => {
         this.setState({ loading: false });
-        this.props.history.push('/')
+        this.props.history.push('/');
       })
       .catch((err) => {
         this.setState({ loading: false });
       });
+  };
+
+  inputChangeHandler = (event, inputId) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+
+    const updatedFormElement = {
+      ...updatedOrderForm[inputId]
+    }
+
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputId] = updatedFormElement;
+
+    this.setState({orderForm: updatedOrderForm})
   }
 
   render() {
+    const formElements = Object.entries(this.state.orderForm).map(
+      ([key, value]) => {
+        return { id: key, config: value };
+      }
+    );
+
     return (
       <div className={classes.ContactInfo}>
         <h4>Please enter your contact info</h4>
         <form>
-          <Input inputtype='input' type='text' name='name' placeholder='your name' />
-          <Input inputtype='input' type='email' name='email' placeholder='your email' />
-          <Input inputtype='input' type='text' name='street' placeholder='street' />
-          <Input inputtype='input' type='text' name='postal' placeholder='postal code' />
-          <Button btnType='Success' clicked={this.orderHandler} >PLACE ORDER</Button>
+          {formElements.map((el) => (
+            <Input
+              key={el.id}
+              elementType={el.config.elementType}
+              elementConfig={el.config.elementConfig}
+              value={el.config.value}
+              handleChange={(event) => this.inputChangeHandler(event, el.id)}
+            />
+          ))}
+          <Button btnType="Success" clicked={this.orderHandler}>
+            PLACE ORDER
+          </Button>
         </form>
       </div>
-    )
+    );
   }
 }
 
